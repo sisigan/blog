318db2525981a765e43136119481fd9a6faf4ab2
sisi <1104602313@qq.com>
2018-12-19 21:55:29 +0800

    文章发表

--- controller/article.js
+++ controller/article.js
@@ -13,7 +13,26 @@ module.exports={
             isLogin:req.session.isLogin,
             userInfo:req.session.userInfo
         })
-    }
-  
+    },
+    postArticleAddHandler(req,res){
+        //获取表单信息
+        console.log(req.body)
+       const articleInfo=req.body;
+        //作者id为session中存放用户信息的id
+       articleInfo.authorid=req.session.userInfo.id;
+       //文章发表时间
+       articleInfo.ctime=moment().format('YYYY-MM-DD HH:mm:ss');
+
+       const sql="insert into blog_articles set ?"
+
+       conn.query(sql,articleInfo,(err,results)=>{
+        if (err || results.affectedRows !=1 ) return res.status(500).send({ status: 500, msg: '文章发表失败!请重试!' })
+
+        res.send({ status: 200, msg: '发表文章成功!',insertId:results.insertId})
+    })
+    },
+    getArticleInfoHandler(req,res){
+
+    },
 
 }
--- routes/article.js
+++ routes/article.js
@@ -2,9 +2,13 @@
 const router =require('../router.js');
 const ctrl =require('../controller/article.js');
 
-//发表文章页
+//get发表文章页
 router.get('/article/add',ctrl.getArticleAddHandler);
 
+//发表文章
+router.post('/article/add',ctrl.postArticleAddHandler);
+
+router.get('/article/info',ctrl.getArticleInfoHandler);
 
 //将路由对象暴露出去
 module.exports=router;
